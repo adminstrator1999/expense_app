b3bac8c89b6f341846b54003d6d987461e99bfb6
bakahtiyor.b@jafton.com <bahtee1997>
2022-02-06 20:24:11 +0500

    webhook is setted

--- bot_core/views.py
+++ bot_core/views.py
@@ -1,11 +1,14 @@
 import os
 
+import telegram
 from django.http import JsonResponse
 from django.views import View
 from telegram.ext import CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackQueryHandler
 
 from .bot import Bot
+import os
 
+PORT = int(os.environ.get('PORT', 88))
 TELEGRAM_URL = "https://api.telegram.org/bot"
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "2003844522:AAGYDJ9SjC1zvqstaQet0bogojYwpH_-I1k")
 
@@ -15,7 +18,9 @@ TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "2003844522:AAGYDJ9SjC1zvqs
 class TutorialBotView(View):
 
     def post(self, request, *args, **kwargs):
-        bot = Bot()
+        # tg_bot = telegram.Bot(TELEGRAM_BOT_TOKEN)
+        # tg_bot.setWebhook('https://9797-185-139-137-249.ngrok.io/expense/' + TELEGRAM_BOT_TOKEN)
+        bot = Bot(TELEGRAM_BOT_TOKEN)
         start_handler = CommandHandler('start', bot.start)
         finish_handler = CommandHandler('finish', bot.finish)
         register_handler = CommandHandler('register_yourself', bot.register_members)
@@ -36,5 +41,13 @@ class TutorialBotView(View):
         bot.dispatcher.add_handler(finish_handler)
         bot.dispatcher.add_handler(register_handler)
         bot.dispatcher.add_handler(message_log_handler)
-        bot.updater.start_polling()
+        bot.updater.start_webhook(listen="0.0.0.0",
+                                  port=PORT,
+                                  url_path=TELEGRAM_BOT_TOKEN)
+        bot.updater.bot.setWebhook('https://expense-bot-kv-app.herokuapp.com/expense/' + TELEGRAM_BOT_TOKEN)
+        # 'https://expense-bot-kv-app.herokuapp.com/'
+        # Run the bot until you press Ctrl-C or the process receives SIGINT,
+        # SIGTERM or SIGABRT. This should be used most of the time, since
+        # start_polling() is non-blocking and will stop the bot gracefully.
+        bot.updater.idle()
         return JsonResponse({"ok": "POST request processed"})
--- expense_app_bot/settings.py
+++ expense_app_bot/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UZT'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
